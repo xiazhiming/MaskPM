bc497d8abda2320396a760cc8d3eb2f4426678c9
xiazhiming <358200167@qq.com>
2020-12-01 16:49:17 +0800

    updates

---
+++ src/.vuepress/config.js
@@ -0,0 +1,117 @@
+module.exports = {
+
+  base: '/MaskPM/',
+
+
+  markdown: {
+    lineNumbers: false // 代码块显示行号
+  },
+  locales: {
+
+    '/': {
+      title: "Mask Product Documentation ",
+      lang: 'zh-CN',
+      description: '项目草稿'
+    }
+  },
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['script', { src: '/js/jquery.slim.min.js' }],
+    ['script', { src: '/js/jquery.fancybox.min.js' }],
+    ['link', { rel: 'stylesheet', type: 'text/css', href: '/css/jquery.fancybox.min.css' }]
+  ],
+  themeConfig: {
+    repo: 'xiazhiming/MaskPM',
+    docsDir: 'src',
+    editLinks: true,
+    subSidebar: 'auto',
+     mode: 'auto', // 默认 auto，auto 跟随系统，dark 暗色模式，light 亮色模式
+    modePicker: false, // 默认 true，false 不显示模式调节按钮，true 则显示
+    // 默认为 "Edit this page"
+    editLinkText: '帮助我们改善此页面！',
+
+
+    locales: {
+
+      '/': {
+        // 多语言下拉菜单的标题
+        selectText: '选择语言',
+        // 该语言在下拉菜单中的标签
+        label: '简体中文',
+
+        // 编辑链接文字
+        editLinkText: '在 GitHub 上编辑此页',
+        // Service Worker 的配置
+        serviceWorker: {
+          updatePopup: {
+            message: "发现新内容可用.",
+            buttonText: "刷新"
+          }
+        },
+   nav: [
+      {
+        text: '设计稿',
+        items: [
+          { text: 'Maskbook', link: 'https://www.figma.com/files/project/4622570/Maskbook' },
+          { text: 'New Dashboard', link: 'https://www.figma.com/files/project/16731522/Mask-Network' }
+        ]
+      }
+    ],
+
+
+        sidebar: {
+          '/app/': [
+            ''
+          ],
+
+          '/pc/': [
+            '', /* /bar/ */
+
+            'Dashboard',
+            'Restore',
+            'Register',
+            'SignIn',
+            'SignUp',
+            'ForgotPassword',
+            'Personas',
+            'Post',
+            'Contacts',
+            'Group',
+            'File',
+            'Wallet',
+            'Transfer',
+            'Swap',
+            'RedPacket',
+            'Buy',
+            'History',
+            'Sale',
+            'Settings',
+            'General',
+            'Backup',
+            'Restore-login',
+            'ChangeEmail',
+            'ChangePassword',
+            'PasswordAuthorization'
+
+
+            
+          ],
+
+          // fallback
+          '/': [
+            '', /* / */
+
+          ]
+
+
+        },
+
+
+        sidebarDepth: 1, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
+        lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间,
+
+      },
+
+    }
+  }
+}
